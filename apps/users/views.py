ec1014fbd17c5a909803fc62587479058b29a756
Alex Soqoor <soqoor@MacBook-Pro-Alex.local>
2020-09-20 21:15:58 +0300

    csrf login fix

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
 import json
 
+@csrf_exempt
 def login_user(request):
     body = json.loads(request.body)
     username = body['username']
